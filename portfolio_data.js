0d972b2d7f91ad71e38b48c803dc0db4a7e2388a
Zach Levitt <zlevitt@middlebury.edu>
2021-03-29 21:48:38 -0400

    starting reengineering of index

---
+++ portfolio_data.js
@@ -0,0 +1,17 @@
+var data = [
+	{
+		imageLink: 'images/earthquake.png',
+		toLink: 'portfolio.html#earthquakes',
+		title: 'Interactive earthquake risk map',
+		organization: 'Los Angeles Times',
+		tools: 'Mapbox GL JS, HTML, CSS, QGIS, Illustrator, Mapshaper',
+	},
+	{
+		imageLink: '',
+		toLink: '',
+		title: '',
+		organization: '',
+		tools: '',
+	},
+
+]
